8e8d4574aef41113733d798ef2f9ece570732b0e
Ali Muhammed <muhammedaliali2020@gmail.com>
2026-08-06 21:30:13 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true // Automatically open browser
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true // Helpful for debugging
-  },
-  resolve: {
-    alias: {
-      '@': '/src' // Create path alias for src directory
-    }
-  }
 })
